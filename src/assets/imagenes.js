24abe21bb05accb2165a3f3c7df105098ae7716f
juanmesa1293 <juanmes@unicauca.edu.co>
2022-07-08 10:42:18 -0500

    Toda la integracion de las ramas

---
+++ src/assets/imagenes.js
@@ -0,0 +1,23 @@
+import logo1 from './images/logo5.png';
+import back1 from './images/back2.png'
+import logoSam from './images/logoSamsung.png'
+import logoUni from './images/logoUnicauca.png'
+import logoAESS from './images/LogoAESS.png'
+import tecno1 from './images/tecno-1.png'
+import tecno2 from './images/tecno-2.png'
+import star1 from './images/star-empty.png'
+import star2 from './images/red-star.png'
+import star3 from './images/yellow-star.png'
+
+export default {
+    "img1": logo1,
+    "img2": back1,
+    "imgSam": logoSam,
+    "imgUni": logoUni,
+    "imgAESS": logoAESS,
+    "imgTec1": tecno1,
+    "imgTec2": tecno2,
+    "star-1": star1,
+    "star-2":star2,
+    "star-3": star3
+}
