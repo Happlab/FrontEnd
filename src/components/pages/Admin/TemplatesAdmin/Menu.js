13e42bd89575020513fa91553cc53b7e39765de5
jeirf12 <jhonfer.ruiz@gmail.com>
2022-12-20 23:36:22 -0500

    fixed small bugs

--- src/components/pages/Admin/TemplatesAdmin/Menu.js
+++ src/components/pages/Admin/TemplatesAdmin/Menu.js
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-import { Button } from "react-bootstrap";
 import user_service from "../../../services/UserServices";
 
 export default class Menu extends Component {
